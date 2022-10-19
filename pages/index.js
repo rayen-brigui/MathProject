9b0bbe009c80721191ef3b5aaa0b3a71c713ebc9
Mr_RB <rayenbrigui92@outlook.fr>
2022-10-19 16:33:03 +0100

    test

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,43 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import { extractSets, generateCombinations, VennDiagram } from '@upsetjs/react';
+ import React,{useMemo} from 'react';
+
+
+
+
+
+function GettingStarted() {
+const elems = useMemo(
+    () => [
+      { name: 'A', sets: ['S1', 'S2'] },
+      { name: 'B', sets: ['S1'] },
+      { name: 'C', sets: ['S2'] },
+      { name: 'D', sets: ['S3'] },
+    ],
+    []
+  );
+  
+  const sets = useMemo(() => extractSets(elems), [elems]);
+const combinations = useMemo(() => generateCombinations(sets), [sets]);
+  const [selection, setSelection] = React.useState(null);
+  return (
+    <VennDiagram
+      sets={sets}
+      combinations={combinations}
+      width={1000}
+      height={700}
+      selection={selection}
+      onHover={setSelection}
+      background={'white'}
+    />
+  );
+}
+
 
 export default function Home() {
+
   return (
     <div className={styles.container}>
       <Head>
@@ -13,43 +48,10 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to <a href="#">Next.js!</a>
         </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        <GettingStarted />
+        
       </main>
 
       <footer className={styles.footer}>
